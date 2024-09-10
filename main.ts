8a08aaf0e08263e63c1cfb2b9fd40c5d36f6b6b7
wwwmello <lunazamorawaltherdavid@gmail.com>
2024-09-09 19:10:00 -0500

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
-let gravedad = 0
-radio.setGroup(60)
-basic.forever(function () {
-    gravedad += 9.81
-})
+on start:
+set masa = 6
+set velocidad = 
+set altura = 1
+set gravedad = 9.81
+
+// Cálculo de energía cinética
+set energiaCinetica = 0.5 * masa * (velocidad * velocidad)
+show number energiaCinetica
+
+// Cálculo de energía potencial
+set energiaPotencial = masa * gravedad * altura
+show number energiaPotencial
